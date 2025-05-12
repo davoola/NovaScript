194c78d90adb98f92c31b9c5ba65d3fe009650c1
davoola <davoola@gmail.com>
2025-05-12 14:52:27 +0800

    统一附件大小限制

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const storage = multer.diskStorage({
 const upload = multer({ 
   storage: storage,
   limits: {
-    fileSize: 50 * 1024 * 1024 // 限制50MB
+    fileSize: 500 * 1024 * 1024 // 限制500MB
   }
 });
 
